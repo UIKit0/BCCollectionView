80dc2b441ed695c6417c26e4b5a9f1e5c537d249
Mike Zornek <mike@clickablebliss.com>
2011-08-18 16:50:50 -0400

    Replaced hard coded magnification velocity formula used during pinch zoom gestures for a more customizable number via delegates.

--- BCCollectionViewDelegate.h
+++ BCCollectionViewDelegate.h
@@ -69,6 +69,8 @@ enum {
 
 //magnifiy events. This method is required BCCollectionView+Zoom is included
 - (NSRange)validScalingRangeForCollectionView:(BCCollectionView *)collectionView;
+- (float)magnificationVelocityForCollectionView:(BCCollectionView *)collectionView; // determines how fast the view should zoom while using the pinch gesture. Can vary depending on how large your validScalingRange is. In my app I have a range of 0..100 and use a magnificationVelocity of (scalingRange.length - scalingRange.location)/2
+
 - (void)colectionViewDidZoom:(BCCollectionView *)collectionView;
 
 //contextual menu
